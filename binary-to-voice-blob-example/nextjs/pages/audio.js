8b8f1168dee62e46bfaf887d64c9e87dfc26c900
Omid Najaee <o.najaee@gmail.com>
2019-01-18 17:03:19 +0100

    read the whole buffer from response stream

--- binary-to-voice-blob-example/nextjs/pages/audio.js
+++ binary-to-voice-blob-example/nextjs/pages/audio.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { parse, stringify, break_by_line } from '../utils/parse';
+import { parse, stringify, break_by_line, read_to_end } from '../utils/parse';
 
 const ENDPOINT = 'https://skill-edge.smartvoicehub.de/cvi/dm/api/v1/invoke/text/multipart?apikey=b507d7ad-9e14-4a26-a3b5-0cc4ec2a2da9';
-const TOKEN = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJwb2MtdGVzdC11c2VyIiwiYXVkIjpbInN2aF9iYWNrZW5kIiwiY3ZpX2NvcmUiLCJ1c2VyX21hbmFnZW1lbnQiLCJldmVudF9oaXN0b3J5Il0sInRyYWNpbmciOmZhbHNlLCJuYmYiOjE1NDc4MDkxNDksInByb2ZpbGUiOiJEZWZhdWx0IiwidGVzdGluZyI6ZmFsc2UsImlzcyI6InVzZXJfbWFuYWdlbWVudCIsImV4cCI6MTU0Nzg5NTU1NCwibG9jYWxlIjoiZGUiLCJpYXQiOjE1NDc4MDkxNTQsInRlbmFudCI6InNtYXJ0aHViX251YW5jZSJ9.OrMlJf8M7EeXYNNzaCMvuksWh8dTFDyWt9VOGu0ZlsM';
+const TOKEN = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJwb2MtdGVzdC11c2VyIiwiYXVkIjpbInN2aF9iYWNrZW5kIiwiY3ZpX2NvcmUiLCJ1c2VyX21hbmFnZW1lbnQiLCJldmVudF9oaXN0b3J5Il0sInRyYWNpbmciOmZhbHNlLCJuYmYiOjE1NDc4MjU2NjUsInByb2ZpbGUiOiJEZWZhdWx0IiwidGVzdGluZyI6ZmFsc2UsImlzcyI6InVzZXJfbWFuYWdlbWVudCIsImV4cCI6MTU0NzkxMjA3MCwibG9jYWxlIjoiZGUiLCJpYXQiOjE1NDc4MjU2NzAsInRlbmFudCI6InNtYXJ0aHViX251YW5jZSJ9.SKvYzqk79_ysrOSXViT3-Kbw0f0G5RFvSyHKOTDZuwM'
 
 export default class extends React.Component {
 	constructor (props) {
@@ -16,8 +16,9 @@ export default class extends React.Component {
 		this.onClickHandler();
 	}
 	
-	onClickHandler = async () =>  {
+	onClickHandler = async () => {
 		try {
+      console.log('req sent');
 			const res = await fetch(ENDPOINT, {
 				method: 'POST',
 				headers: {
@@ -28,36 +29,36 @@ export default class extends React.Component {
 				},
 				body: '{"text": "Wie ist das Wetter in Frankfurt?"}',
 			});
-      const text = await res.text();
-      console.log('resp', text.length);
+      /* const text = await res.text();
+       * console.log('resp', text); */
       
       const contentType = res.headers.get('content-type');
       const boundary = `--${contentType.split(';')[1].split('=')[1]}`;
 
       const reader = res.body.getReader();
-      const { value, done }  = await reader.read();
-      const lines = break_by_line(value);
+      const lines = await read_to_end(reader);
       const audio = lines.slice(9).flat();
-      
-      /// this doesn't work
-      // const buffer = new Uint8Array(audio.flat());
-      // const audioCtx = new AudioContext();
-      // audioCtx.decodeAudioData(buffer.buffer, function(data) {
-      // }, (err) => console.log(err));
 
-      // create the audio buffer manually
-      const audioCtx = new AudioContext();
-      const targetSampleRate = 16000;
+      console.log('audio', audio);
+      /// this doesn't work
+      /* const buffer = new Uint8Array(audio);
+       * const audioCtx = new AudioContext();
+       * audioCtx.decodeAudioData(buffer.buffer, function(data) {
+       * }, (err) => console.log(err)); */
       
+      // create the audio buffer manually
+
+      const audioCtx = new AudioContext();
+      const targetSampleRate = 16000; 
       const audioBuffer = audioCtx.createBuffer(1, audio.length, targetSampleRate);
       const channel = audioBuffer.getChannelData(0);
       for (let i = 0; i < audio.length; i++) {
-        const index = Math.round(targetSampleRate * i);
-        channel[index] = audio[i] / 128. - 1 // Math.sin(audio[i] * Math.PI * 0.5)  // this has to be a number in [-1., 1.]
+        //const index = Math.round(targetSampleRate * i); 
+        channel[i] = audio[i] / 128. - 1 // Math.sin(audio[i]  Math.PI  0.5)  // this has to be a number in [-1., 1.]
       }
       const source = audioCtx.createBufferSource();
       source.buffer = audioBuffer;
-      source.connect(audioCtx.destination);
+      source.connect(audioCtx.destination); 
       source.start();
 		} catch (err) {
 			console.log('err', err);
--- binary-to-voice-blob-example/nextjs/utils/parse.js
+++ binary-to-voice-blob-example/nextjs/utils/parse.js
@@ -1,5 +1,5 @@
 const LINE_BREAK = '\n';
-const multipart = require('parse-multipart');
+// const multipart = require('parse-multipart');
 
 export const stringify = (bytes) => {
   let str = '';
@@ -7,6 +7,19 @@ export const stringify = (bytes) => {
     str += String.fromCharCode(bytes[i]);
   }
   return str;
+}
+
+export const read_to_end = async (reader) => {
+  let lines = [];
+  while (true) {
+    const { value, done }  = await reader.read();
+    if (done) {
+      break;
+    } else {
+      lines = lines.concat(break_by_line(value));
+    }
+  }
+  return lines;
 }
 
 export const break_by_line = (value) => {
@@ -24,6 +37,7 @@ export const break_by_line = (value) => {
   return lines;
 }
 
+/// dumb way of parsing the response
 export const parse = (text) => {
 	const delim = text.split(LINE_BREAK)[0].substr(2);
 	const parts = text.split(delim).slice(1, 3);
@@ -41,7 +55,8 @@ export const parse = (text) => {
 	}
 
   return new Uint8Array(bytes);
-  
+  /// first attempt: this usually works with base64 or
+  ///                binary encoded data but is not the case here
 	// const blob = new Blob([new Uint8Array(bytes)], { type: 'audio/wav' });
 	// console.log('blob', blob);
 	// return blob; 
