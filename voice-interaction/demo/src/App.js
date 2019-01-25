545616cd892524d0c93f653e8a9ab656c9946a21
Omid Najaee <o.najaee@gmail.com>
2019-01-25 16:03:10 +0100

    add voice command perception basic example

--- voice-interaction/demo/src/App.js
+++ voice-interaction/demo/src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import api from 'sound-api';
+import styles from './App.css';
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit
-        <code>src/App.js</code>
-        and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+const options = {
+  'go-up': () => {
+    console.log('scroll up');
+    window.scrollBy(0, -500);
+  },
+  'go-down': () => {
+    console.log('scroll down');
+    window.scrollBy(0, 500);
+  },
+};
+
+const sound = api(options);
+
+class App extends Component {
+  componentDidMount() {
+    sound.start();
+  }
+
+  componentWillUnmount() {
+    sound.stop();
+  }
+
+  render() {
+    return (
+      <div className={styles.App}>
+        <p>Aliquam posuere.</p>
+        <p>Nullam libero mauris, consequat quis, varius et, dictum id, arcu.</p>
+        <p>Vivamus id enim.</p>
+        <p>Curabitur lacinia pulvinar nibh.</p>
+        <p>Donec pretium posuere tellus.</p>
+        <p>Aliquam feugiat tellus ut neque.</p>
+        <p>Donec posuere augue in quam.</p>
+      </div>
+    );
+  }
+}
 
 export default App;
